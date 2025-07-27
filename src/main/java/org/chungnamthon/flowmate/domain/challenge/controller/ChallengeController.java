c3bc40086af13ea67dfe02dd38a050b1f102a5cd
kwakseobang <iii6602@naver.com>
2025-07-27 18:18:21 +0900

    [#1] Setting: Challenge 기본 구조 셋팅

---
+++ src/main/java/org/chungnamthon/flowmate/domain/challenge/controller/ChallengeController.java
@@ -0,0 +1,10 @@
+package org.chungnamthon.flowmate.domain.challenge.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class ChallengeController {
+
+}
