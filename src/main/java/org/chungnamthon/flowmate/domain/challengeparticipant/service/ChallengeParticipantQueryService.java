3c6bb53a05f48d4874b15339a5417fe6dc7c16cd
kwakseobang <iii6602@naver.com>
2025-07-27 18:22:28 +0900

    [#1] Setting: ChallengeParticipant 기본 구조 셋팅

---
+++ src/main/java/org/chungnamthon/flowmate/domain/challengeparticipant/service/ChallengeParticipantQueryService.java
@@ -0,0 +1,13 @@
+package org.chungnamthon.flowmate.domain.challengeparticipant.service;
+
+import lombok.RequiredArgsConstructor;
+import org.chungnamthon.flowmate.domain.challengeparticipant.repository.ChallengeParticipantRepository;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class ChallengeParticipantQueryService {
+
+    private final ChallengeParticipantRepository challengeParticipantRepository;
+
+}
