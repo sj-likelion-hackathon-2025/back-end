c8f6a345660233b86b1998d688324e1b857db4bb
kwakseobang <iii6602@naver.com>
2025-07-27 17:37:47 +0900

    [#1] Setting: ChallengeParticipant Entity 초기 설정

---
+++ src/main/java/org/chungnamthon/flowmate/domain/challengeparticipant/entity/ChallengeRole.java
@@ -0,0 +1,8 @@
+package org.chungnamthon.flowmate.domain.challengeparticipant.entity;
+
+public enum ChallengeRole {
+
+    LEADER, // 리더
+    GENERAL // 일반
+
+}
---
+++ src/test/java/org/chungnamthon/flowmate/domain/challengeparticipant/entity/ChallengeParticipantTest.java
@@ -0,0 +1,48 @@
+package org.chungnamthon.flowmate.domain.challengeparticipant.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.chungnamthon.flowmate.domain.challenge.entity.Challenge;
+import org.chungnamthon.flowmate.domain.challenge.entity.ChallengeFixture;
+import org.chungnamthon.flowmate.domain.member.entity.Member;
+import org.chungnamthon.flowmate.domain.member.entity.MemberFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ChallengeParticipantTest {
+
+    @DisplayName("챌린지 리더를 생성한다.")
+    @Test
+    void createLeader() {
+        var member = MemberFixture.createMember();
+        var challenge = ChallengeFixture.createChallenge();
+
+        var leader = ChallengeParticipant.createLeader(member, challenge);
+
+        assertThat(leader.getRole()).isEqualTo(ChallengeRole.LEADER);
+    }
+
+    @DisplayName("챌린지 일반 참여자를 생성한다.")
+    @Test
+    void createGeneral() {
+        var member = MemberFixture.createMember();
+        var challenge = ChallengeFixture.createChallenge();
+
+        var general = ChallengeParticipant.createGeneral(member, challenge);
+
+        assertThat(general.getRole()).isEqualTo(ChallengeRole.GENERAL);
+    }
+
+    @DisplayName("챌린지 참여자의 권한을 변경한다.")
+    @Test
+    void updateRole() {
+        var member = MemberFixture.createMember();
+        var challenge = ChallengeFixture.createChallenge();
+        var general = ChallengeParticipant.createGeneral(member, challenge);
+
+        general.updateRole(ChallengeRole.LEADER);
+
+        assertThat(general.getRole()).isEqualTo(ChallengeRole.LEADER);
+    }
+}
