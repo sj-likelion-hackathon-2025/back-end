f9960337f4dc58e8f4760ec5e1b2c9dd0aa90d45
kwakseobang <iii6602@naver.com>
2025-07-27 12:24:31 +0900

    git initizlize

---
+++ src/main/java/org/chungnamthon/flowmate/FlowmateApplication.java
@@ -0,0 +1,13 @@
+package org.chungnamthon.flowmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowmateApplication.class, args);
+	}
+
+}
