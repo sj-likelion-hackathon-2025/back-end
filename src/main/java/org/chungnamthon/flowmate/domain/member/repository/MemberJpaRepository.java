630b989a8bfeef51dc8adde691e6c6a803536c13
kwakseobang <iii6602@naver.com>
2025-07-28 15:19:38 +0900

    [#7] Feat: Member 생성 구현

--- src/main/java/org/chungnamthon/flowmate/domain/member/repository/MemberJpaRepository.java
+++ src/main/java/org/chungnamthon/flowmate/domain/member/repository/MemberJpaRepository.java
@@ -1,8 +1,15 @@
 package org.chungnamthon.flowmate.domain.member.repository;
 
+import java.util.Optional;
 import org.chungnamthon.flowmate.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberJpaRepository extends JpaRepository<Member,Long> {
+
+    Optional<Member> findBySocialId(String socialId);
+
+    boolean existsByName(String name);
+
+    Optional<Member> findByRefreshToken(String token);
 
 }
--- src/main/java/org/chungnamthon/flowmate/domain/member/repository/MemberRepository.java
+++ src/main/java/org/chungnamthon/flowmate/domain/member/repository/MemberRepository.java
@@ -1,6 +1,10 @@
 package org.chungnamthon.flowmate.domain.member.repository;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.chungnamthon.flowmate.domain.member.entity.Member;
+import org.chungnamthon.flowmate.global.exception.NotFoundException;
+import org.chungnamthon.flowmate.global.exception.dto.ErrorStatus;
 import org.springframework.stereotype.Repository;
 
 @RequiredArgsConstructor
@@ -8,5 +12,27 @@ import org.springframework.stereotype.Repository;
 public class MemberRepository {
 
     private final MemberJpaRepository memberJpaRepository;
+
+    public void save(Member member) {
+        memberJpaRepository.save(member);
+    }
+
+    public Member findById(Long id) {
+        return memberJpaRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_MEMBER));
+    }
+
+    public Optional<Member> findBySocialId(String socialId) {
+        return memberJpaRepository.findBySocialId(socialId);
+    }
+
+    public boolean existsByName(String name) {
+        return memberJpaRepository.existsByName(name);
+    }
+
+    public Member findByRefreshToken(String refreshToken) {
+        return memberJpaRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new NotFoundException(ErrorStatus.NOT_FOUND_TOKEN));
+    }
 
 }
