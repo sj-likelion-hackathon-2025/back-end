10104522e8545cf5cfd82f1e27dbc05582182a23
kwakseobang <iii6602@naver.com>
2025-07-28 15:20:29 +0900

    [#7] Setting: OAuth 설정 추가

--- src/main/java/org/chungnamthon/flowmate/global/security/config/SecurityConfig.java
+++ src/main/java/org/chungnamthon/flowmate/global/security/config/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.chungnamthon.flowmate.global.security.jwt.JwtAccessDeniedHandler;
 import org.chungnamthon.flowmate.global.security.jwt.JwtAuthenticationEntryPoint;
 import org.chungnamthon.flowmate.global.security.jwt.JwtFilter;
 import org.chungnamthon.flowmate.global.security.jwt.JwtProvider;
+import org.chungnamthon.flowmate.global.security.oauth2.handler.CustomOAuth2FailureHandler;
+import org.chungnamthon.flowmate.global.security.oauth2.handler.CustomOAuth2SuccessHandler;
+import org.chungnamthon.flowmate.global.security.oauth2.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,6 +28,9 @@ public class SecurityConfig {
 
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final CustomOAuth2SuccessHandler customSuccessHandler;
+    private final CustomOAuth2FailureHandler customOauth2FailureHandler;
     private final JwtProvider jwtProvider;
     private final ObjectMapper objectMapper;
 
@@ -41,6 +47,14 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**")
                         .permitAll()
+                );
+
+        http
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(customSuccessHandler)
+                        .failureHandler(customOauth2FailureHandler)
                 );
 
         http
