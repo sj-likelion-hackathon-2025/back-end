1c99bf8369fa37c8a192318e5be763270aa98bec
kwakseobang <iii6602@naver.com>
2025-07-28 15:19:38 +0900

    [#7] Feat: Member 생성 구현

--- src/main/java/org/chungnamthon/flowmate/domain/member/service/MemberCommandService.java
+++ src/main/java/org/chungnamthon/flowmate/domain/member/service/MemberCommandService.java
@@ -1,13 +1,21 @@
 package org.chungnamthon.flowmate.domain.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.chungnamthon.flowmate.domain.member.entity.Member;
 import org.chungnamthon.flowmate.domain.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class MemberCommandService {
 
     private final MemberRepository memberRepository;
+
+    @Transactional
+    public void updateRefreshToken(Long memberId, String refreshToken) {
+        Member member = memberRepository.findById(memberId);
+        member.updateRefreshToken(refreshToken);
+    }
 
 }
