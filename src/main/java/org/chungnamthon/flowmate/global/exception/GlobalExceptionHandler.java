b735467fb318d6e45f8b3b0b1a0fc14fc0e029ed
kwakseobang <iii6602@naver.com>
2025-07-29 12:56:14 +0900

    [#9] Chore: 미사용 import 제거
    [#9] Feat: AuthMember custom 어노테이션 생성

--- src/main/java/org/chungnamthon/flowmate/global/exception/GlobalExceptionHandler.java
+++ src/main/java/org/chungnamthon/flowmate/global/exception/GlobalExceptionHandler.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @Slf4j
 @RestControllerAdvice
